8cf85d1718aeb31f78475a4fdb0c485601aebbd2
Dmytro Soroka <soroka.dmytro@pdffiller.team>
2019-01-18 13:56:40 +0200

    [ARCH-291] change Dispatcher class

--- src/ServiceProviders/EventServiceProvider.php
+++ src/ServiceProviders/EventServiceProvider.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 namespace AirSlate\Datadog\ServiceProviders;
 
 use AirSlate\Datadog\Listeners\EventBusListener;
+use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Events\Dispatcher;
 
 /**
  * Class EventServiceProvider
