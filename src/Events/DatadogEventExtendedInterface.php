b48d9f207f530508521e7cf1902da4e594f174ce
Volynets Ivan <volynets.ivan@pdffiller.team>
2020-07-30 12:09:35 +0300

    added different metrics to custom events

---
+++ src/Events/DatadogEventExtendedInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AirSlate\Datadog\Events;
+
+/**
+ * interface DatadogEventAbstract
+ */
+interface DatadogEventExtendedInterface extends DatadogEventInterface
+{
+    public const METRIC_TYPE_HISTOGRAM = 'histogram';
+    public const METRIC_TYPE_INCREMENT = 'increment';
+    public const METRIC_TYPE_DECREMENT = 'decrement';
+    public const METRIC_TYPE_GAUGE = 'gauge';
+    public const METRIC_TYPE_TIMING = 'timing';
+
+    /**
+     * Value for your metric, return 1 as default.
+     * @return int
+     */
+    public function getValue(): int;
+
+    /**
+     * metric type you can see in constants of this interface
+     * @return string
+     */
+    public function getMetricType(): string;
+}
